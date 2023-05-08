f30ce5464c8713c5ec6060a5bff6c7c4a29ca7c9
--add <raghav1107.be21@chitkara,edu.in>
2023-05-08 17:08:47 +0530

    remve from playlist correction query upadted to body 3

--- controllers/userController.js
+++ controllers/userController.js
@@ -222,7 +222,7 @@ export const addToPlaylist = catchAsyncError(async (req, res, next) => {
 export const removeFromPlaylist = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
 
-  const course = await Course.findById(req.body.id);
+  const course = await Course.findById(req.query.id);
 
   if (!course) return next(new ErrorHandler("Invalid Course id", 404));
 
